5d6e1518ef6f3fd8cb8060eb73fbd6d96b6b33d7
erichamers <erichamers@gmail.com>
2020-11-16 02:27:47 -0300

    Commit

--- pull.sh
+++ pull.sh
@@ -6,6 +6,8 @@ QTILE_DIR=$CONFIG_DIR"qtile/"
 QUTEBROWSER_DIR=$CONFIG_DIR"qutebrowser/"
 VIM_DIR=$CONFIG_DIR"vim/"
 ZSH_DIR=$CONFIG_DIR"zsh/"
+I3_DIR=$CONFIG_DIR"i3/"
+POLYBAR_DIR=$CONFIG_DIR"polybar/"
 
 # Source config  
 ROOT_CONF=$HOME"/.config/"
@@ -13,8 +15,12 @@ QTILE_FILE=$ROOT_CONF"qtile/config.py"
 QUTEBROWSER_FILE=$ROOT_CONF"qutebrowser/config.py"
 VIM_FILE=$HOME"/.vimrc"
 ZSH_FILE=$HOME"/.zshrc"
+I3_FILE=$ROOT_CONF"/i3/config"
+I3_SCRIPT1=$ROOT_CONF"/i3/launch_modules.sh"
+I3_SCRIPT2=$ROOT_CONF"/i3/launch_top.sh"
+POLYBAR_FILE=$ROOT_CONF"/polybar/config"
 
-sourceDirs=($CONFIG_DIR $QTILE_DIR $QUTEBROWSER_DIR $VIM_DIR $ZSH_DIR)
+sourceDirs=($CONFIG_DIR $QTILE_DIR $QUTEBROWSER_DIR $VIM_DIR $ZSH_DIR $I3_DIR $POLYBAR_DIR)
 
 echo "Creating folder structure"
 if ! [ -d "$CONFIG_DIR" ]; then
@@ -43,3 +49,11 @@ cp -f $VIM_FILE $VIM_DIR
 
 echo "==== Zsh ===="
 cp -f $ZSH_FILE $ZSH_DIR
+
+echo "==== I3 ===="
+cp -f $I3_FILE $I3_DIR
+cp -f $I3_SCRIPT1 $I3_DIR
+cp -f $I3_SCRIPT2 $I3_DIR
+
+echo "==== Polybar ===="
+cp -f $POLYBAR_FILE $POLYBAR_DIR
