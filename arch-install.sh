f16a50effce9a0cbefb460ad139ad0e3372f9987
erichamers <erichamers@gmail.com>
2020-11-23 16:19:50 -0300

    Commit

--- arch-install.sh
+++ arch-install.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
+# Author: Eric Hamers (erichamers@gmail.com)
+# This is the installation file for my arch linux i3-gaps environment.
+# It assumes that the instalation was done via zen-installer and a couple of 
+# applications are preinstalled.
+# Preinstalled applications that might be relative to this installation file
+# are: 
+#   - fish
+#   - yay
+#   - git
+# plus some other base applications and firmware
+
 HOME=~eric
 PROJECT_DIR=$HOME/"projects/dotfiles"
 CONFIG_DIR=$HOME/".config"
 
 echo "Creating folder structure..."
-mkdir $HOME/projects $HOME/apps $CONFIG_DIR/picom $HOME/.ssh
+mkdir $HOME/projects $HOME/apps $CONFIG_DIR/picom $HOME/fonts $HOME/.ssh
 
 echo "Installing Vundle plugin manager for vim..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -13,8 +24,12 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git config --global user.name "erichamers"
 git config --global user.email "erichamers@gmail.com"
 
+echo "Installing powerline fonts..."
+git clone https://github.com/powerline/fonts ~/fonts/
+cd $HOME/fonts/ && ./install.sh
+
 echo "Installing basic packages..." 
-pacman -S qutebrowser \
+yay -S qutebrowser \
     picom \
     alacritty \
     powerline \
@@ -29,10 +44,10 @@ pacman -S qutebrowser \
     neofetch \
     unclutter \
     pcmanfm \
-    xclip
-
-echo "Installing polybar..."
-yay -S polybar skypeforlinux-stable-bin visual-studio-code-bin
+    xclip \
+    polybar \
+    skypeforlinux-stable-bin \
+    visual-studio-code-bin
 
 echo "Installing Python..."
 pyenv install 3.8.6
@@ -46,5 +61,9 @@ cp -r $PROJECT_DIR/"polybar" $CONFIG_DIR
 cp -r $PROJECT_DIR/"fish" $CONFIG_DIR
 cp -r $PROJECT_DIR/"vim/.vimrc" $HOME
 cp -r $PROJECT_DIR/"color-scripts" $CONFIG_DIR
+cp -r $PROJECT_DIR/"alacritty" $CONFIG_DIR
+
+echo "Customizing alacritty..."
+curl https://raw.githubusercontent.com/dracula/alacritty/master/dracula.yml >> $CONFIG_DIR/alacritty/alacritty.yml
 
 echo "Installation complete!"
