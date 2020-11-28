b04fcccaf0f70edfd552355b6d9279bf650478e1
erichamers <erichamers@gmail.com>
2020-11-27 22:27:59 -0300

    Commit

--- arch-install.sh
+++ arch-install.sh
@@ -18,9 +18,10 @@ CONFIG_DIR=$HOME/".config"
 echo "Creating folder structure..."
 mkdir $HOME/projects $HOME/apps $CONFIG_DIR/picom $HOME/fonts $HOME/.ssh
 
+yay -S openssh
 ssh-keygen -t ed25519 -C "erichamers@gmail.com" -N '' -f $HOME/.ssh/id_ed25519
 eval "$(ssh-agent -s)"
-ssh-add $CONFIG_DIR/.ssh/id_ed25519
+ssh-add $HOME/.ssh/id_ed25519
 
 echo "Installing Vundle plugin manager for vim..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
--- bash/.bashrc
+++ bash/.bashrc
@@ -2,8 +2,13 @@
 # ~/.bashrc
 #
 
+neofetch
+
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
 alias ls='ls --color=auto'
+alias ll='ls -l'
+alias l='ls -alh'
+alias copy='xclip -sel clip'
 PS1='[\u@\h \W]\$ '
