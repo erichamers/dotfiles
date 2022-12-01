6e50486d017d63502bec90d85f08b90a8c25651c
erichamers <erichamers@gmail.com>
2022-12-01 15:05:20 -0300

    stuf"

--- nvim/lua/eric/keymaps.lua
+++ nvim/lua/eric/keymaps.lua
@@ -9,16 +9,18 @@ nnoremap("<C-w>", "<cmd>bd<CR>")
 nnoremap("<C-k>", "<cmd>bnext<CR>")
 nnoremap("<C-j>", "<cmd>bprev<CR>")
 
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
-nnoremap('<C-p>', builtin.find_files)
-nnoremap('<leader>fg', builtin.live_grep)
-nnoremap('<leader>fb', builtin.buffers)
-nnoremap('<leader>fh', builtin.help_tags)
+nnoremap("<C-p>", builtin.find_files)
+nnoremap("<leader>fg", builtin.live_grep)
+nnoremap("<leader>fb", builtin.buffers)
+nnoremap("<leader>fh", builtin.help_tags)
 
 -- Move between splits
 nnoremap("<C-h>", "<C-W>h")
 nnoremap("<C-l>", "<C-W>l")
 
+-- nnoremap("<C-t>", "<cmd>Ex<CR>")
+-- nnoremap("<C-T>", "<cmd>Rex<CR>")
 -- Nvim-Tree keymaps
 nnoremap("<C-t>", "<cmd>NvimTreeToggle<CR>")
--- nvim/lua/eric/nvim_tree.lua
+++ nvim/lua/eric/nvim_tree.lua
@@ -6,25 +6,25 @@ vim.opt.termguicolors = true
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  hijack_cursor = true,
-  actions = {
-      change_dir = {
-          restrict_above_cwd = true
-      }
-  },
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort_by = "case_sensitive",
+	hijack_cursor = true,
+	actions = {
+		change_dir = {
+			restrict_above_cwd = true,
+		},
+	},
+	view = {
+		adaptive_size = true,
+		mappings = {
+			list = {
+				{ key = "u", action = "dir_up" },
+			},
+		},
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
